e5ba5d1a27886c371f098cf189a865321c62abac
Piero <pieroviscone@gmail.com>
2023-06-01 20:00:00 +0200

    Modified test

--- test/dataset.py
+++ test/dataset.py
@@ -3,8 +3,12 @@ import uproot
 import torch
 import numpy as np
 import awkward as ak
+from torch.utils.data import Dataset
+import torch
+import sys
 
-f=uproot.open("BigMuons_MuonCuts.root")
-tree = f["Events"]
 
+f=uproot.open("../../Preselection_Skim/powheg/TTSemiLept_powheg_MuonCuts.root")
+t=f["Events"]
+#arr=t.arrays("Masses")["Masses"].to_numpy(allow_missing=False)
 # %%
