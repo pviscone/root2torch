534e3a52096a8a62a84ef3308a47d9a701439a47
Piero <pieroviscone@gmail.com>
2023-06-01 22:44:24 +0200

    Modified test

--- test/dataset.py
+++ test/dataset.py
@@ -6,9 +6,9 @@ import awkward as ak
 from torch.utils.data import Dataset
 import torch
 import sys
-
+sys.path.append("../Root2Torch")
+from torch_dataset import EventsDataset
 
 f=uproot.open("../../Preselection_Skim/powheg/TTSemiLept_powheg_MuonCuts.root")
 t=f["Events"]
-#arr=t.arrays("Masses")["Masses"].to_numpy(allow_missing=False)
-# %%
+arr=t.arrays("Masses")["Masses"].to_numpy(allow_missing=False)
