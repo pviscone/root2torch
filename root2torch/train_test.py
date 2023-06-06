8c556a18f862740a145e2c285507a1c6de2aeccd
Piero <pieroviscone@gmail.com>
2023-06-06 03:33:38 +0200

    train_test script from scratch

--- root2torch/train_test.py
+++ root2torch/train_test.py
@@ -7,34 +7,32 @@ from torch_dataset import EventsDataset
 #%%
 #Load signal and background datasets
 signal=torch.load("../../Preselection_Skim/signal/signal_MuonCuts.pt")
-powheg=torch.load("../../Preselection_Skim/powheg/TTSemiLept_powheg_MuonCuts.pt")
+semiLept=torch.load("../../Preselection_Skim/powheg/TTSemiLept_MuonCuts.pt")
+diLept=torch.load("../../Preselection_Skim/diLept/TTdiLept_MuonCuts.pt")
 signal.shuffle()
-powheg.shuffle()
+semiLept.shuffle()
+diLept.shuffle()
+#%%
 n_sig=len(signal)
+n_train=int(n_sig*0.85)
 
+#Build training and test datasets
+train_signal=signal.get_batch(0,n_train)
+train_semiLept=semiLept.get_batch(0,n_train*3)
+train_diLept=diLept.get_batch(0,n_train)
+train_dataset=signal.cat(train_diLept, retrieve=True)
+train_dataset=train_dataset.cat(train_semiLept, retrieve=True)
+train_dataset.shuffle()
 
-
-#Select only muons in the background and add to the signal
-lepton_powheg=powheg.mask(torch.abs(powheg.data["LeptLabel"])==13, retrieve=True)
-powheg_nn=lepton_powheg.get_batch(0,n_sig*4)
-nn=signal.cat(powheg_nn, retrieve=True)
-
-
-#Add the rest of the mu background to the non muon background
-other_lepton=lepton_powheg.get_batch(n_sig*4,-1)
-non_lepton_powheg=powheg.mask(torch.abs(powheg.data["LeptLabel"])!=13, retrieve=True)
-other_dataset=other_lepton.cat(non_lepton_powheg, retrieve=True)
-
-#Shuffle the datasets
-other_dataset.shuffle()
-nn.shuffle()
-
-#create train and test datasets
-test_size=0.15
-train_dataset=nn.get_batch(0,int((1-test_size)*len(nn)))
-test_dataset=nn.get_batch(int((1-test_size)*len(nn)),-1)
-
+test_signal=signal.get_batch(n_train,-1)
+test_semiLept=semiLept.get_batch(n_train*3,-1)
+test_diLept=diLept.get_batch(n_train,-1)
+test_dataset=signal.cat(test_diLept, retrieve=True)
+test_dataset=test_dataset.cat(test_semiLept, retrieve=True)
+test_dataset.shuffle()
 
 torch.save(train_dataset,"../../Preselection_Skim/NN/train_Muons.pt")
 torch.save(test_dataset,"../../Preselection_Skim/NN/test_Muons.pt")
-torch.save(other_dataset,"../../Preselection_Skim/NN/TTSemilept_MuonCuts.pt")
+
+
+# %%
