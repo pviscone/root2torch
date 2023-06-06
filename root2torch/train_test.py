e1f01411d02ab9a69de537ee259fb65eef8690c2
Piero <pieroviscone@gmail.com>
2023-06-06 05:14:37 +0200

    splitted test dataset

--- root2torch/train_test.py
+++ root2torch/train_test.py
@@ -25,19 +25,26 @@ n_train=int(n_sig*0.85)
 train_signal=signal.get_batch(0,n_train)
 train_semiLept=semiLept.get_batch(0,n_train*3)
 train_diLept=diLept.get_batch(0,n_train)
-train_dataset=signal.cat(train_diLept, retrieve=True)
+train_dataset=train_signal.cat(train_diLept, retrieve=True)
 train_dataset=train_dataset.cat(train_semiLept, retrieve=True)
-train_dataset.shuffle()
 
 test_signal=signal.get_batch(n_train,-1)
-test_semiLept=semiLept.get_batch(n_train*3,-1)
-test_diLept=diLept.get_batch(n_train,-1)
-test_dataset=signal.cat(test_diLept, retrieve=True)
+test_semiLept=semiLept.get_batch(n_train*3,n_train*3+3*len(test_signal))
+test_diLept=diLept.get_batch(n_train,n_train+len(test_signal))
+test_dataset=test_signal.cat(test_diLept, retrieve=True)
 test_dataset=test_dataset.cat(test_semiLept, retrieve=True)
+
+other_semilept=semiLept.get_batch(n_train*3+3*len(test_signal),-1)
+other_diLept=diLept.get_batch(n_train+len(test_signal),-1)
+others=other_semilept.cat(other_diLept, retrieve=True)
+
+train_dataset.shuffle()
 test_dataset.shuffle()
+others.shuffle()
 
 torch.save(train_dataset,"../../Preselection_Skim/NN/train_Muons.pt")
 torch.save(test_dataset,"../../Preselection_Skim/NN/test_Muons.pt")
-
+torch.save(others,"../../Preselection_Skim/NN/OtherBkg_Muons.pt")
+torch.save(test_signal,"../../Preselection_Skim/NN/test_signal_Muons.pt")
 
 # %%
