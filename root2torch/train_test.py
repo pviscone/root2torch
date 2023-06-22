9ba9f388e62dab414c2082a4e1d70eb2c3e2ff60
Piero <pieroviscone@gmail.com>
2023-06-22 16:12:15 +0200

    simpler train_test dataset builder

--- root2torch/train_test.py
+++ root2torch/train_test.py
@@ -6,41 +6,25 @@ from torch_dataset import EventsDataset
 
 #%%
 #Load signal and background datasets
-signal=torch.load("../../Preselection_Skim/signal/signal_MuonCuts.pt")
-semiLept=torch.load("../../Preselection_Skim/powheg/TTSemiLept_MuonCuts.pt")
-diLept=torch.load("../../Preselection_Skim/diLept/TTdiLept_MuonCuts.pt")
+signal_train=torch.load("../../Preselection_Skim/NN/train/torch/signal_train_MuonCuts.pt")
+semiLept_train=torch.load("../../Preselection_Skim/NN/train/torch/TTSemiLept_train_MuonCuts.pt")
+diLept_train=torch.load("../../Preselection_Skim/NN/train/torch/TTdiLept_train_MuonCuts.pt")
 
-signal.shuffle()
-semiLept.shuffle()
-diLept.shuffle()
-#%%
-n_sig=len(signal)
-n_train=int(n_sig*0.85)
 
-#Build training and test datasets
-train_signal=signal.get_batch(0,n_train)
-train_semiLept=semiLept.get_batch(0,n_train*4)
-train_diLept=diLept.get_batch(0,n_train)
-train_dataset=train_signal.cat(train_diLept, retrieve=True)
-train_dataset=train_dataset.cat(train_semiLept, retrieve=True)
+signal_test=torch.load("../../Preselection_Skim/NN/test/torch/signal_test_MuonCuts.pt")
+semiLept_test=torch.load("../../Preselection_Skim/NN/test/torch/TTSemiLept_test_MuonCuts.pt")
+diLept_test=torch.load("../../Preselection_Skim/NN/test/torch/TTdiLept_test_MuonCuts.pt")
 
-test_signal=signal.get_batch(n_train,-1)
-test_semiLept=semiLept.get_batch(n_train*4,n_train*4+4*len(test_signal))
-test_diLept=diLept.get_batch(n_train,n_train+len(test_signal))
-test_dataset=test_signal.cat(test_diLept, retrieve=True)
-test_dataset=test_dataset.cat(test_semiLept, retrieve=True)
+train_dataset=signal_train.cat(diLept_train, retrieve=True)
+train_dataset=train_dataset.cat(semiLept_test, retrieve=True)
 
-other_semilept=semiLept.get_batch(n_train*4+4*len(test_signal),-1)
-other_diLept=diLept.get_batch(n_train+len(test_signal),-1)
-others=other_semilept.cat(other_diLept, retrieve=True)
-
+test_dataset=signal_test.cat(diLept_test, retrieve=True)
+test_dataset=test_dataset.cat(semiLept_test, retrieve=True)
 train_dataset.shuffle()
 test_dataset.shuffle()
-others.shuffle()
+
 
 torch.save(train_dataset,"../../Preselection_Skim/NN/train_Muons.pt")
 torch.save(test_dataset,"../../Preselection_Skim/NN/test_Muons.pt")
-torch.save(others,"../../Preselection_Skim/NN/OtherBkg_Muons.pt")
-torch.save(test_signal,"../../Preselection_Skim/NN/test_signal_Muons.pt")
 
 # %%
